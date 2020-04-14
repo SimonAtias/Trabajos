0e8646289277092b7ac5533b1dbe181c0cf5361e
SimonAtias <57919753+SimonAtias@users.noreply.github.com>
2020-04-14 00:19:50 -0300

    Create Main.java

---
+++ Marzo/25/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        RegistroDeDatos rdd1= new RegistroDeDatos();
+        RegistroDeCompras rdc1= new RegistroDeCompras();
+        
+        rdd1.inserts();
+        rdd1.mostrar();
+        rdc1.inserts();
+        rdc1.mostrar();
+
+    }
+}
