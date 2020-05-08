6ba3a63a384e2daf1e8e4507c24b3f4cbfdacaf6
SimonAtias <57919753+SimonAtias@users.noreply.github.com>
2020-05-08 15:05:56 -0300

    Add files via upload

---
+++ Marzo/27/Jugador.java
@@ -0,0 +1,19 @@
+public class Jugador {
+
+    String nombre;
+    String apellido;
+    String edad;
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getEdad() {
+        return this.edad;
+    }
+
+}
